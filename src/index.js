5236295b6e455ee866ce3f37e715ace64b77e4b2
Liam Tinney <ltinney@powerhrg.com>
2020-07-21 09:49:37 -0400

    Add fetchToys

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -13,3 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetchToys()
+// When the page loads, make a 'GET' request to fetch all the toy objects. With the
+// response data, make a `<div class="card">` for each toy and add it to the
+// toy-collection `div`.
+
+// DOM content load, you'll need a fetch to get all toy objects
+// fetch
+// find HTML elements to refer to
+
+function fetchToys() {
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(function(toys) {
+    console.log(toys)
+  })
+}
